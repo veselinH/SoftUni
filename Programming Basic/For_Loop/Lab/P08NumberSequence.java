f17818db25b296be4a3ff2892635d9569e1f848c
Veselin Hristov <v.h.hristov7@gmail.com>
2022-09-12 17:19:15 +0300

    Added exercise

---
+++ Programming Basic/For_Loop/Lab/P08NumberSequence.java
@@ -0,0 +1,26 @@
+package For_Loop.Lab;
+
+import java.util.Scanner;
+
+public class P08NumberSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int maxNumber = Integer.MIN_VALUE;
+        int minNumber = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number > maxNumber) {
+                maxNumber = number;
+            }
+            if (number < minNumber) {
+                minNumber = number;
+            }
+        }
+        System.out.printf("Max number: %d%n", maxNumber);
+        System.out.printf("Min number: %d%n", minNumber);
+    }
+}
