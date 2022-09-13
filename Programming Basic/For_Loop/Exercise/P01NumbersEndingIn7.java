d3569acba2b1ad385b2ca1828ceb0421c5a20ae5
Veselin Hristov <v.h.hristov7@gmail.com>
2022-09-13 09:53:17 +0300

    Added exercise

---
+++ Programming Basic/For_Loop/Exercise/P01NumbersEndingIn7.java
@@ -0,0 +1,12 @@
+package For_Loop.Exercise;
+
+public class P01NumbersEndingIn7 {
+    public static void main(String[] args) {
+
+        for (int i = 7; i <= 997; i++) {
+            if (i % 10 == 7) {
+                System.out.println(i);
+            }
+        }
+    }
+}
