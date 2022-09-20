48bad37d51d12f490fd412a1e19c8dbd9a0aebfc
Veselin Hristov <v.h.hristov7@gmail.com>
2022-09-20 17:02:40 +0300

    Added exercise

---
+++ Programming Basic/While_Loop/Exercise/P04Walking.java
@@ -0,0 +1,28 @@
+package While_Loop.Exercise;
+
+import java.util.Scanner;
+
+public class P04Walking {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int goal = 10000;
+        int steps = 0;
+        while (goal > steps) {
+            String input = scanner.nextLine();
+            if (input.equals("Going home")) {
+                steps += Integer.parseInt(scanner.nextLine());
+                break;
+            }
+            steps += Integer.parseInt(input);
+        }
+        if (steps >= goal) {
+            System.out.printf("Goal reached! Good job!\n" +
+                    "%d steps over the goal!", steps - goal);
+        } else {
+            System.out.printf("%d more steps to reach goal.", goal - steps);
+
+        }
+    }
+}
