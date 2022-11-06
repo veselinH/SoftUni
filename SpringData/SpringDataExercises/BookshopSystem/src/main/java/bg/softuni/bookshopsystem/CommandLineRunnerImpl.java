81991d778465f69cb855018024426ec22a3f779d
Veselin Hristov <v.h.hristov7@gmail.com>
2022-11-06 17:40:30 +0200

    Created Bookshop System

---
+++ SpringData/SpringDataExercises/BookshopSystem/src/main/java/bg/softuni/bookshopsystem/CommandLineRunnerImpl.java
@@ -0,0 +1,13 @@
+package bg.softuni.bookshopsystem;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
+}
